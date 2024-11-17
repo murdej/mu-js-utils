cc38e6f3355a97fe91d68769fb43bc0aabb15510
Vít Peprníček <vit.peprnicek@gmail.com>
2024-11-17 11:23:02 +0100

    ...

---
+++ CHANGES.md
@@ -0,0 +1,9 @@
+# Murdej JS utils
+
+## v1.0.0
+
+ - function `sleepPromise`
+
+## v1.1.0
+
+ - function `escapeHtml`
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
 export function sleepPromise(duration: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, duration));
 }
+
+export function escapeHtml(src: string): string {
+    return src
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
